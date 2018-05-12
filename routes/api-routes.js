e703fa5b409d012e36fcd77d29d22d2ef2693ceb
Holly Voigts <hollysvoigts@gmail.com>
2018-05-12 11:51:28 -0500

    updated the auth routes to api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -41,3 +41,32 @@ module.exports = function(app) {
 
 //   });
 };
+//routes for sign up, sign in and log out
+var authController = require('../controllers/apiController.js');
+
+module.exports = function(app, passport) {
+    
+app.get('/signup', authController.signup);
+
+
+//app.get('/main', authController.signin);  //signin or main
+
+app.post('/signup', passport.authenticate('local-signup', {successRedirect: '/profile', 
+                                                            failureRedirects: '/main'} //main not signup since signup modal is on main
+                                                            ));
+
+//app.get('/profile' isLoggedIn, authController.profile); 
+
+//app.get('/logout', authController.logout);
+
+//app.post('/main', passport.authenticate('local-signin', {successRedirect: '/profile', 
+                              //                          failureRedirect: '/main'}
+                              //                          ));
+//function isLoogedIn (req, res, next) {
+ //   if (req.isAuthenticated())
+ //       return next();
+ //   res.redirect('/signin');
+//}
+}
+
+
