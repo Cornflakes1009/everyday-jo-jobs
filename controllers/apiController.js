c7490f7e7a619b89e96c7d56a02befc227c027a0
Holly Voigts <hollysvoigts@gmail.com>
2018-05-12 12:25:36 -0500

    api controller corrected

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -14,33 +14,7 @@ module.exports = getName;
 
 // each api route will have its own func in the api controller
 
-//authcontroller 
-var authController = require('../controllers/apiController.js');
 
-module.exports = function(app, passport) {
-    
-app.get('/signup', authController.signup);
-
-
-//app.get('/main', authController.signin);  //signin or main
-
-app.post('/signup', passport.authenticate('local-signup', {successRedirect: '/profile', 
-                                                            failureRedirects: '/main'} //main not signup since signup modal is on main
-                                                            ));
-
-//app.get('/profile' isLoggedIn, authController.profile); 
-
-//app.get('/logout', authController.logout);
-
-//app.post('/main', passport.authenticate('local-signin', {successRedirect: '/profile', 
-                              //                          failureRedirect: '/main'}
-                              //                          ));
-//function isLoogedIn (req, res, next) {
- //   if (req.isAuthenticated())
- //       return next();
- //   res.redirect('/signin');
-//}
-}
 
                                         
 
