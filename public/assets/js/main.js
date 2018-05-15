f3351e311742c7e01fa7aff9f02dd1086dadab9a
Harold Davidson <harold.davidson89@gmail.com>
2018-05-14 20:20:19 -0500

    fixed merge conflict

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,20 +1,13 @@
 $(document).ready(function () {
   $('#loginModal').modal({ backdrop: 'static', keyboard: false });
 
+  $("#modal-sign-up").on("click", function () {
+    window.location.href = "/signup";
+  });
+
   var tr = $('<tr>');
   var $membersContainer = $(".table-body");
   var members = [];
-
-
-  // function initializeRows() {
-  //   $membersContainer.empty();
-  //   var rowsToAdd = [];
-  //   for (var i = 0; i < members.length; i++) {
-  //     rowsToAdd.push(createNewRow(members[i]));
-  //   }
-  //   $membersContainer.append(rowsToAdd);
-  // }
-
 
   function getUsers() {
     $.get("/api/members", function (data) {
@@ -22,7 +15,7 @@ $(document).ready(function () {
       members = data;
       console.log(members);
       var rowsToAdd = [];
-      for (var i = 0; i < members.length ; i++) {
+      for (var i = 0; i < members.length; i++) {
         rowsToAdd.push(
           '<tr>',
           `<td>${members[i].name}</td>`,
@@ -38,24 +31,9 @@ $(document).ready(function () {
         );
       }
       $membersContainer.append(rowsToAdd);
-      // initializeRows();
-      // console.log(members[0].name);
+
     });
   };
-
-  // function createNewRow(todo) {
-  //   var $newInputRow = $(
-  //     '<tr>'
-  //   );
-
-    // $newInputRow.find("button.delete").data("id", todo.id);
-    // $newInputRow.find("input.edit").css("display", "none");
-    // $newInputRow.data("todo", todo);
-    // if (todo.complete) {
-    //   $newInputRow.find("span").css("text-decoration", "line-through");
-    // }
-    // return $newInputRow;
-  //}
 
   getUsers();
 }); // end of document ready func
